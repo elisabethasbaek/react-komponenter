2bf46721d146694d481bf7ac23cf6166c1463ae8
elisabethasbaek <elisabeth.asbaek@gmail.com>
2021-01-13 09:28:39 +0100

    add collections finished styling

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -4,8 +4,8 @@ function Collection({category, image, authorimage, heading, author}) {
     return (
         <>
             <article className="collection">
-                <p className="collection__category">{category}</p>
                 <img src={image} alt="" className="collection__image"/>
+                <p className="collection__category">{category}</p>
                 <img src={authorimage} alt="" className="collection__authorimage"/>
                 <h1 className="collection__heading">{heading}</h1>
                 <p className="collection__author">{author}</p>
